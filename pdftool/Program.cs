50f74fbc0a893a37ac9c1e4e3a24dc5214f1e9f4
mgrau67 <mgrau67@gmail.com>
2020-06-03 21:07:46 +0200

    Initial commit

---
+++ pdftool/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace pdftool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Usage();
+                return;
+            }
+
+            string opt = args[0];
+            if (opt == "extract" && args.Length == 5)
+            {
+                string ifile = args[1];
+                string ofile = args[2];
+                int.TryParse(args[3], out int from);
+                int.TryParse(args[4], out int to);
+                var ipdf = PdfReader.Open(ifile, PdfDocumentOpenMode.Import);
+                var opdf = new PdfDocument();
+                for (int i = from; i <= to; ++i)
+                    opdf.AddPage(ipdf.Pages[i]);
+                opdf.Save(ofile);
+            }
+            else
+                Usage();
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("usage: extract <in-pdf> <out-pdf> <from> <to>");
+        }
+    }
+}
